adec4e6590d99a09f4f0663c8e5c9f51d239bffe
rakesh <rakeshshankar2k16@gmail.com>
2024-10-19 19:19:27 +0530

    corrections

--- setupcypressautomationfw.sh
+++ setupcypressautomationfw.sh
@@ -1,10 +1,10 @@
 #install node which installs npm. To verify run 'node -v && npm -v'
 #brew install node@20
-mkdir frontend_automation_framework
-cd frontend_automation_framework
+mkdir frontend_automation_framework1
+cd frontend_automation_framework1
 npm init -y
 #mkdir /tmp/cypress_cache
-mkdir /Users/rakesh/cypress_cache
+mkdir /Users/rakeshhb/Downloads/cypress_cache
 #export CYPRESS_CACHE_FOLDER="/tmp/cypress_cache"
 export CYPRESS_CACHE_FOLDER="/Users/rakeshhb/Downloads/cypress_cache"
 npm install cypress --save-dev
